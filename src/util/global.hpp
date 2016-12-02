2e0561eec584a54bc1935d9c5439912f2d6bf417
Jeff Hammond <jeff.science@gmail.com>
2016-12-02 15:03:31 +0800

    add macro that translates MPI thread level int to string

--- src/util/global.hpp
+++ src/util/global.hpp
@@ -7,6 +7,13 @@
 
 #include <omp.h>
 
+#include "mpi.h"
+#define MPI_THREAD_STRING(level)  \
+    ( level==MPI_THREAD_SERIALIZED ? "THREAD_SERIALIZED" : \
+        ( level==MPI_THREAD_MULTIPLE ? "THREAD_MULTIPLE" : \
+            ( level==MPI_THREAD_FUNNELED ? "THREAD_FUNNELED" : \
+                ( level==MPI_THREAD_SINGLE ? "THREAD_SINGLE" : "THREAD_UNKNOWN" ) ) ) )
+
 #include "config.h"
 
 #include "lawrap.hpp"
